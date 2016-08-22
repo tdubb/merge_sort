fb67fba403bc5c2d89373c69d16c6c836d8a54cd
JDjedi <JDjedi1201@gmail.com>
2016-08-22 12:41:16 -0500

    Amended mergesort.rb

--- mergeSort.rb
+++ mergeSort.rb
@@ -1,28 +1,25 @@
 @array = [55, 3, 40, 45, 23, 12, 28, 90, 7, 27]
+@merge_array = []
 
 def sort(arr)
 	if arr.length != 2
 		new_array = arr.each_slice(arr.length/2).to_a
-		arr_a = new_array[0].sort!
-		arr_b = new_array[1].sort!
-		merge(arr_a, arr_b)
+		@arr_a = new_array[0].sort!
+		@arr_b = new_array[1].sort!
 	end
 end
 
 def merge(arg, arg1)
-	merge_array = []
-
-	if merge_array.length < @array.length
-		if arg[x] < arg1[y]
-			merge_array.push(arg[x])
-			merge(arr_a, arr_b)
-		else
-			merge_array.push(arg1[y])
-			merge(arr_a, arr_b)
-		end
-	else
+	if  @merge_array.length == @array.length
 		puts "#{merge_array}"
+	elsif arg[x] < arg1[y]
+		@merge_array.push(arg[x])
+		merge(arr_a, arr_b)
+	else arg[x] > arg1[y]
+		@merge_array.push(arg1[y])
+		merge(arr_a, arr_b)
 	end
 end
 
 sort(@array)
+(@array.length).times { |x, y| merge(@arr_a, @arr_b) }
